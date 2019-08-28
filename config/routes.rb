bf4753856618282d98002548f1550937ef36a31b
duguangting <dgt_x@foxmail.com>
2019-08-28 15:45:27 +0800

    finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  get '/signup', to: "users#new"
 
-  get 'static_pages/about'
+  # get 'static_pages/home'
+  get '/home', to: "static_pages#home"
 
-  get 'static_pages/contact'
+  # get 'static_pages/help'
+  get '/help', to: "static_pages#help"
+
+  # get 'static_pages/about'
+  get '/about', to: "static_pages#about"
+
+  # get 'static_pages/contact'
+  get '/contact', to: "static_pages#contact"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
